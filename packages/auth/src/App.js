80ec5d367af715de193e7a681fcadeaa7fe47c00
Yurii Grynkov <yugrinkov@gmail.com>
2021-12-02 13:06:52 +0200

    Added subapp

---
+++ packages/auth/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Switch, Route, Router } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/styles";
+
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import SignIn from "./components/Signin";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "au",
+});
+
+export default ({ history, onSignIn }) => {
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+            <Route path="/auth/signin/">
+              <SignIn onSignIn={onSignIn} />
+            </Route>
+            <Route path="/auth/signup/">
+              <Signup onSignIn={onSignIn} />
+            </Route>
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
+};
--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,8 +1,8 @@
-import { mount } from "marketing/MarketingApp";
+import { mount } from "auth/AuthApp";
 import React, { useRef, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
@@ -14,6 +14,9 @@ export default () => {
         if (pathname !== nextPathname) {
           history.push(nextPathname);
         }
+      },
+      onSignIn: () => {
+        onSignIn();
       },
     });
 

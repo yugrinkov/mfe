d8dfa6e0ca88666556d4b7a7289e76fe0446548c
Yurii Grynkov <yugrinkov@gmail.com>
2021-12-02 17:01:01 +0200

    trigger pipeline

--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -4,6 +4,7 @@ import Dashboard from "./components/Dashboard.vue";
 const mount = (el) => {
   const app = createApp(Dashboard);
   app.mount(el);
+  console.log("mounted");
 };
 
 if (process.env.NODE_ENV === "development") {
